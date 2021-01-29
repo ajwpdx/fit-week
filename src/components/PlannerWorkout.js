54c41e8cc52d01f2e90b4b7677af4a52ffaa6d9b
Alex Williams <alexjwilliams1@gmail.com>
2021-01-29 15:24:56 -0800

    styling touchups

--- src/components/PlannerWorkout.js
+++ src/components/PlannerWorkout.js
@@ -8,7 +8,7 @@ const PlannerWorkout = (props) => {
             <div className={`planner-workout time cat-${props.workout.category}`}>
                 <h4>{props.workout.title}</h4>
                 <p><strong>Time:</strong> {props.workout.time}</p>
-                <p><strong>Notes:</strong> {props.workout.notes}</p>
+                <p>{props.workout.notes}</p>
             </div>
         )
     } else if (props.workout.distance != null) {
@@ -16,20 +16,24 @@ const PlannerWorkout = (props) => {
             <div className={`planner-workout distance cat-${props.workout.category}`}>
                 <h4>{props.workout.title}</h4>
                 <p><strong>Distance:</strong> {`${props.workout.distance} ${props.workout.distanceMeasurement} `}</p>
-                <p><strong>Notes:</strong> {props.workout.notes}</p>
+                <p> {props.workout.notes}</p>
             </div>
         )
     } else if (props.workout.setsOfReps != null) {
         return (
             <div className={`planner-workout setOfReps cat-${props.workout.category}`}>
                 <h4>{props.workout.title}</h4>
-                <p ><strong>Sets:</strong> {props.workout.time}</p>
+                <p><strong>Sets:</strong> {props.workout.time}</p>
                 <div className='sets-container'>
-                {props.workout.setsOfReps.map((set, index) => {
-                    return <p className='sets'>{`${index+1}: ${set.reps} x ${set.weight} lbs`}</p>
-                })}
+                    {props.workout.setsOfReps.map((set, index) => {
+                        return (
+                            <div className='set'>
+                                <p className='set-number'>{`${index + 1}`}</p>
+                                <p className='set-details'>{`${set.reps} x ${set.weight} lbs`}</p>
+                            </div>)
+                    })}
                 </div>
-                <p><strong>Notes:</strong> {props.workout.notes}</p>
+                <p>{props.workout.notes}</p>
             </div>
         )
     } else {
