069f8722633b5d4bf78b395fc132b63d27d69980
Alex Williams <alexjwilliams1@gmail.com>
2021-02-03 20:24:16 -0800

    added search bar to AddWorkouts component

--- src/components/SavedWorkouts.js
+++ src/components/SavedWorkouts.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const SavedWorkouts = (props) => {
+
+    const [search, setSearch] = useState('')
+
+    const onChange = (evt) => {
+        setSearch(evt.target.value)
+    }
 
     return (
         <section id='saved-workouts'>
             <h2>My Workouts</h2>
+            <form>
+                <input
+                placeholder='Find workouts'
+                name='search'
+                type='text'
+                value={search}
+                onChange={onChange}
+                />
+            </form>
         </section>
     )
 }
