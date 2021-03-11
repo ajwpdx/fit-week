07a5830502e5311b43675b7f6817cb4add5a76c2
Alex Williams <alexjwilliams1@gmail.com>
2021-03-11 15:32:41 -0800

    added to create workout

--- src/components/views/CreateWorkout.js
+++ src/components/views/CreateWorkout.js
@@ -7,9 +7,27 @@ const CreateWorkout = (props) => {
     return (
 
         <div id="create-workout">
-            <Header/>
-            <h2>Create Workout</h2>
-            <WorkoutForm/>
+            <Header />
+            <div className='create-workout'>
+                <h2>Create Workout</h2>
+                <form className='create-workout-form'>
+                    <div className='topline-inputs'>
+                    <div className='new-wo-title'>
+                        <label>Title</label>
+                        <input />
+                    </div>
+                    <div className='new-wo-date'>
+                        <label>Date</label>
+                        <input />
+                    </div>
+                    <div className='new-wo-category'>
+                        <label>Category</label>
+                        <input />
+                    </div>
+                    </div>
+                </form>
+            </div>
+
         </div>
 
     )
